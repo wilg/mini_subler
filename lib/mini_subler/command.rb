a27738e3a91313eeae298b58612a6a9b1878f4e2
Wil Gieseler <supapuerco@gmail.com>
2012-08-19 14:08:35 -0700

    setting metadata

--- lib/mini_subler/command.rb
+++ lib/mini_subler/command.rb
@@ -29,6 +29,43 @@ module MiniSubler
     rescue
       nil
     end
+    
+    def set_metadata(file_path, hash)
+      file_path = File.expand_path file_path
+      line = Cocaine::CommandLine.new(self.command_path, "-dest :source -metadata :metadata", source: file_path, metadata: subler_hash_to_argument(hash_to_subler_hash(hash)))
+      puts line.command
+      puts line.run
+    rescue
+      nil
+    end
+        
+    def hash_to_subler_hash(h)
+      n = {}
+      h.each do |key, value|
+        n[key.to_s.titleize.gsub("Tv", "TV").gsub("Number", "#")] = value
+      end
+      n
+    end
+    
+    def subler_hash_to_hash(h)
+      n = {}
+      h.each do |tag_name, tag_value|
+        n[tag_name.gsub("#", "Number").parameterize("_").to_sym] = tag_value
+      end
+      n
+    end
+    
+    def subler_hash_to_argument(h)
+      s = ""
+      h.each do |key, value|
+       s << "{#{escape key}:#{escape value}}" 
+      end
+      s
+    end
+    
+    def escape(s)
+      s.gsub("{", "&#123;").gsub("{", "&#125;").gsub(":", "&#58;")
+    end
 
   end
   
